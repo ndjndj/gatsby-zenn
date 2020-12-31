e905e7e691c9df941d140ce5698f45ff3eee1c19
ndjndj <teq.ndj@gmail.com>
2021-01-01 00:32:49 +0900

    until chapter17

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,25 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetaData: {
+    title: "Dev Blog",
+    description: "blog site created with Gatsby",
+    author: "ndj"
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY
+      }
+    }
+  ],
+
 }
